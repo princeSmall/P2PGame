b72dc26e7b7a0138a55c18957e0c46bc60f9252f
Tongle <Tongle@sansi.com>
2016-04-18 16:47:36 +0800

    game

---
+++ P2PGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  P2PGame
+//
+//  Created by 童乐 on 12-12-17.
+//  Copyright (c) 2016年 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ P2PGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  P2PGame
+//
+//  Created by 童乐 on 12-12-17.
+//  Copyright (c) 2016年 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
